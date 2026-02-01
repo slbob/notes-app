f0450775aefb3c4d1cef687b4857c08a639a5cf6
slbob <44273800+slbob@users.noreply.github.com>
2026-02-01 10:30:19 -0800

    Update noteRoutes.js

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,8 +1,10 @@
 const r = require("express").Router();
 const a = require("../middleware/auth");
 const c = require("../controllers/noteController");
-r.get("/",a,c.getNotes);
-r.post("/",a,c.createNote);
-r.put("/:id",a,c.updateNote);
-r.delete("/:id",a,c.deleteNote);
+
+r.get("/", a, c.getNotes);
+r.post("/", a, c.createNote);
+r.put("/:id", a, c.updateNote);
+r.delete("/:id", a, c.deleteNote);
+
 module.exports = r;
