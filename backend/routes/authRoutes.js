4ed1757af7c7bc3b8198a228b727e7af1a7888f2
slbob <44273800+slbob@users.noreply.github.com>
2026-02-01 10:30:00 -0800

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,7 @@
 const r = require("express").Router();
 const c = require("../controllers/authController");
-r.post("/register",c.register);
-r.post("/login",c.login);
+
+r.post("/register", c.register);
+r.post("/login", c.login);
+
 module.exports = r;
