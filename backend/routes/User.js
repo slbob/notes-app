055d302238a348ffedf5c313a41009e3d498059c
slbob <44273800+slbob@users.noreply.github.com>
2026-02-01 10:19:00 -0800

    Delete backend/routes/User.js

--- backend/routes/User.js
+++
@@ -1,7 +0,0 @@
-const db = require("../database/db");
-exports.createUser = (email, password) => new Promise((res, rej) =>
-  db.run("INSERT INTO users (email, password) VALUES (?,?)",[email,password],function(e){e?rej(e):res(this.lastID);})
-);
-exports.findUserByEmail = (email) => new Promise((res, rej) =>
-  db.get("SELECT * FROM users WHERE email=?",[email],(e,row)=>e?rej(e):res(row))
-);
