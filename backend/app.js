37812cf42c69b1c11a799aaec1982759546ebca6
slbob <44273800+slbob@users.noreply.github.com>
2026-02-01 10:10:44 -0800

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/auth",require("./routes/authRoutes"));
+app.use("/notes",require("./routes/noteRoutes"));
+module.exports = app;
