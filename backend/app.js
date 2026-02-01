656d6c36526c748ea7768e1336d1eb0f5ce96423
slbob <44273800+slbob@users.noreply.github.com>
2026-02-01 10:23:29 -0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/auth",require("./routes/authRoutes"));
-app.use("/notes",require("./routes/noteRoutes"));
+
+app.use("/auth", require("./routes/authRoutes"));
+app.use("/notes", require("./routes/noteRoutes"));
+
 module.exports = app;
